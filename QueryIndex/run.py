805c3d819aec1cc52ad32b90056dee6fa0d534d7
carl <tianboxiong@ucsb.edu>
2022-01-30 17:09:13 -0600

    update run.pu

--- QueryIndex/run.py
+++ QueryIndex/run.py
@@ -10,6 +10,7 @@ score = [" BM25"] #" BM25"
 random_bmw_size = " /share/msmarco-passage/variable-bmw/deepimpact.sizes"
 random_bmw_size2 = " /share/msmarco-passage/variable-bmw/regular/deeplayered.sizes"
 random_bmw_size3 = " /share/msmarco-passage/variable-bmw/bm25.sizes"
+random_bmw_size4 = " /share/msmarco-passage/variable-bmw/bm25L/bm25L.sizes "
 
 command = "./query_binary_index /share/msmarco/msmarco_passage_v1.normlen /share/msmarco-passage/bm25_count.data /share/msmarco-passage/bm25_count.info /share/msmarco-passage/queries.dev.tsv"
 command2 = "./query_binary_index /share/msmarco/msmarco_passage_v1.normlen /share/msmarco-passage/deepimpact.data /share/msmarco-passage/deepimpact.info /share/msmarco-passage/queries.dev.tsv"
@@ -26,7 +27,7 @@ for i in method:
                 f = open("/home/carl/SIGIR2021-ShortPaper-WindowNavigation/QueryIndex/variable_bmw_result/" + i.strip() + "_"+ b.strip() + "_"  + p.strip() + "_" + s.strip() + ".txt","w+")
                 if(score == " BM25"):
                     # if(x == " NV"):
-                    subprocess.run(command+i+b+p+s+" invalidate_address",shell=True,stdout=f)
+                    subprocess.run(command4+i+b+p+s+" invalidate_address",shell=True,stdout=f)
                     # else:
                     #     subprocess.run(command+i+b+p+s+random_bmw_size,shell=True,stdout=f)
                 # else:
@@ -34,6 +35,6 @@ for i in method:
                     # subprocess.run(command2+i+b+p+s+" invalidiate_address", shell=True,stdout=f)
                     # else:
                     # subprocess.run(command3+i+b+p+s+" invalidate_address", shell=True,stdout=f)
-            # f_1 = open("/home/carl/SIGIR2021-ShortPaper-WindowNavigation/QueryIndex/variable_bmw_result/" + i.strip() + "_"+ "0" + "_"  + p.strip() + "_" + s.strip() + ".txt","w+")
-            # subprocess.run(command+i+" 0"+p+s+random_bmw_size3, shell=True,stdout=f_1)
+            f_1 = open("/home/carl/SIGIR2021-ShortPaper-WindowNavigation/QueryIndex/variable_bmw_result/" + i.strip() + "_"+ "0" + "_"  + p.strip() + "_" + s.strip() + ".txt","w+")
+            subprocess.run(command4+i+" 0"+p+s+random_bmw_size3, shell=True,stdout=f_1)
 
